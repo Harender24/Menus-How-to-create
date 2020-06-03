08aa4578b549b26606cbe194865b983010daf48d
Harry <Harender_24@hotmail.com>
2020-06-03 08:10:45 +0530

    Tabs folder added

---
+++ Tabs/app.js
@@ -0,0 +1,18 @@
+function openCity(e,cityName){
+    let tabContent=document.querySelectorAll('.tab-content');
+    let tabLinks=document.querySelectorAll('.tab-links');
+    console.log(tabContent);
+      console.log(tabLinks);
+    
+    for(let i=0;i<tabContent.length;i++){
+      tabContent[i].style.display='none';
+    }
+     for(let i=0;i<tabLinks.length;i++){
+      tabLinks[i].classList.remove('active');
+    }
+    
+    document.querySelector(`#${cityName}`).style.display='block';
+    console.log(e);
+    e.classList.add('active');
+    
+  }
